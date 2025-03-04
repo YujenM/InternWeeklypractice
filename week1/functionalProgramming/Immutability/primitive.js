d27470cc6f1caf8428e2d642d354a0c9848164f8
YujenM <maharjanyuzen@gmail.com>
2025-03-04 14:04:31 +0545

    Immutable=Primitive completed

---
+++ week1/functionalProgramming/Immutability/primitive.js
@@ -0,0 +1,18 @@
+// string
+let text='Yujen';
+text[0]="B";
+console.log("First console:"+text); //output Yujen 
+
+text="Batman"
+console.log("Second Console: "+text);// output Batman
+
+// Number
+let num=10;
+num=num+5;
+console.log(num); //output 15 The original 10 did not change. Instead, a new number 15 was created, and num now refers to this new value
+
+
+let istrue=true;
+istrue=false;
+console.log(istrue); //output false The value true was not changed. Instead, a new value false was assigned.
+
