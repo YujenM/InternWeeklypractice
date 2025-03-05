e7ef7f326b9f2a07c344aac986a1df7a0c3b274c
YujenM <maharjanyuzen@gmail.com>
2025-03-05 12:04:27 +0545

    Immutable=pureclass completed

---
+++ week1/functionalProgramming/pureFunction/pfunction.js
@@ -0,0 +1,15 @@
+const purefun=(a,b)=>{
+    return a+b;
+}
+
+console.log(purefun(5,10));
+console.log(purefun(5,10));
+
+let total=0;
+const inpurefun=(num)=>{
+    total=total+num
+    return total
+}
+
+console.log(inpurefun(19));
+console.log(inpurefun(12));
